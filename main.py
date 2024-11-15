aa5b8833f4de82c0bdc4d595915d5c894b620b35
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-15 14:09:45 +0430

    Update main.py

--- main.py
+++ main.py
@@ -233,8 +233,8 @@ def check_number_callback(call: ClassVar[Any]) -> NoReturn:
     # Directly call the /number command handler function
     number_command_handler(call.message)
 
-@bot.message_handler(commands=["vip_number", "usage"])
-def help_command_handler(message: ClassVar[Any]) -> NoReturn:
+@bot.callback_query_handler(func=lambda call: call.data == "vip_number")
+def check_numb_callback(call):
     """
     Function to handle help commands in bot
     Shows help messages to users
