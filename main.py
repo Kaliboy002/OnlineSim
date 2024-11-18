59aa7dc94c5a1f561b82be6e19bc20d42b949770
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-18 23:33:58 +0430

    Update main.py

--- main.py
+++ main.py
@@ -787,16 +787,18 @@ def process_add_one_user(message):
             points = int(message.text)
             user_id = int(user_identifier) if user_identifier.isdigit() else None
 
-  if user_id and user_id in user_ids:
-                referral_data[user_id] = referral_data.get(user_id, 0) + points
-                bot.send_message(
-                    chat_id=user_id,
-                    text=(f" <b>😚 {points} invites added to your account  ★</b>\n"
+if user_id and user_id in user_ids:
+    referral_data[user_id] = referral_data.get(user_id, 0) + points
+    bot.send_message(
+        chat_id=user_id,
+        text=(
+            f"<b>😚 {points} invites added to your account ★</b>\n"
             "┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n"
-            " <b>😚 به حساب شما {points} دعوت اضافه شد ★</b>"
+            f"<b>😚 به حساب شما {points} دعوت اضافه شد ★</b>"
         ),
-                parse_mode="HTML"
-                )
+        parse_mode="HTML"
+    )
+
 
 
 
