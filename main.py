bbde9d81207f7d3a7b9b7947e0d996ce31ac1709
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-20 18:13:21 +0430

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def language_selection_callback(call):
         keyboard = types.InlineKeyboardMarkup(row_width=1)
         keyboard.add(
             types.InlineKeyboardButton("Jᴏɪɴ ᴄʜᴀɴɴᴇʟ 𝟷⚡️", url="https://t.me/Kali_Linux_BOTS"),
-            types.InlineKeyboardButton("Jᴏɪɴ ᴄʜᴀɴɴᴇʟ 2⚡️", url="https://t.me/PAWSOG_bot/PAWS?startapp=vHumU732"),
+            types.InlineKeyboardButton("Jᴏɪɴ ᴄʜᴀɴɴᴇʟ 2⚡️", url="https://youtube.com/@afghantech001"),
             types.InlineKeyboardButton("🔐𝗝𝗼𝗶𝗻𝗲𝗱", callback_data="check_numb")
         )
 
@@ -143,7 +143,7 @@ def language_selection_callback(call):
         keyboard = types.InlineKeyboardMarkup(row_width=1)
         keyboard.add(
             types.InlineKeyboardButton("عضو در کانال اول⚡️", url="https://t.me/Kali_Linux_BOTS"),
-            types.InlineKeyboardButton("عضو در کانال دوم⚡️", url="https://t.me/PAWSOG_bot/PAWS?startapp=vHumU732"),
+            types.InlineKeyboardButton("عضو در کانال دوم⚡️", url="https://youtube.com/@afghantech001"),
             types.InlineKeyboardButton("🔐 عضـو شـدم", callback_data="check_numbf")
         )
 
